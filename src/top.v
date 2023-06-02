3b43b5329e29a40a86adab71f51b229744f37775
Daniel J Wisehart <dwisehart@gmail.com>
2023-06-02 12:42:52 -0600

    Remove 197.

--- src/top.v
+++ src/top.v
@@ -27,18 +27,18 @@ module tt_um_greycode_top #( parameter pTEST = 'b0 )
 ////////////////////////////////////////
    wire [5:0] w_sel;
    wire       w_clk_005, w_clk_011, w_clk_023, w_clk_047, w_clk_097, w_clk_197;
-   ringd #( .pSTAGES(   5 ) )
-     m_ring005d( .i_clk( clk ), .i_sel( w_sel[5] ), .o_clk( w_clk_005 ) );
-   ringd #( .pSTAGES(  11 ) )
-     m_ring011d( .i_clk( clk ), .i_sel( w_sel[4] ), .o_clk( w_clk_011 ) );
-   ringd #( .pSTAGES(  23 ) )
-     m_ring023d( .i_clk( clk ), .i_sel( w_sel[3] ), .o_clk( w_clk_023 ) );
-   ringd #( .pSTAGES(  47 ) )
-     m_ring047d( .i_clk( clk ), .i_sel( w_sel[2] ), .o_clk( w_clk_047 ) );
+//   ringd #( .pSTAGES(   5 ) )
+//     m_ring005d( .i_clk( clk ), .i_sel( w_sel[5] ), .o_clk( w_clk_005 ) );
+//   ringd #( .pSTAGES(  11 ) )
+//     m_ring011d( .i_clk( clk ), .i_sel( w_sel[4] ), .o_clk( w_clk_011 ) );
+//   ringd #( .pSTAGES(  23 ) )
+//     m_ring023d( .i_clk( clk ), .i_sel( w_sel[3] ), .o_clk( w_clk_023 ) );
+//   ringd #( .pSTAGES(  47 ) )
+//     m_ring047d( .i_clk( clk ), .i_sel( w_sel[2] ), .o_clk( w_clk_047 ) );
    ringd #( .pSTAGES(  97 ) )
      m_ring097d( .i_clk( clk ), .i_sel( w_sel[1] ), .o_clk( w_clk_097 ) );
-   ringd #( .pSTAGES( 197 ) )
-     m_ring197d( .i_clk( clk ), .i_sel( w_sel[0] ), .o_clk( w_clk_197 ) );
+//   ringd #( .pSTAGES( 197 ) )
+//     m_ring197d( .i_clk( clk ), .i_sel( w_sel[0] ), .o_clk( w_clk_197 ) );
    // Next primes in this sequence are 397, 797, 1597
 
 ////////////////////////////////////////
