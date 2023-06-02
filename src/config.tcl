063b6c72f37da60540983eb0e159f4436768d3e4
Daniel J Wisehart <dwisehart@gmail.com>
2023-06-02 06:48:16 -0600

    Set density 0.35.

--- src/config.tcl
+++ src/config.tcl
@@ -47,6 +47,7 @@ set ::env(CLOCK_TREE_SYNTH) 1
 # period is in ns, so 20ns == 50mHz
 set ::env(CLOCK_PERIOD) "20"
 set ::env(CLOCK_PORT) {clk}
+set ::env(PL_TARGET_DENSITY) 0.35
 
 # hold/slack margin
 # set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.8
